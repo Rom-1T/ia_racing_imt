aad1d3189bae95bd273123fcb116b7c4308e0b76
ambrosios <ambrosios@mail.com>
2023-02-07 10:53:17 +0100

    Entrainement modele supervise 2 datasets

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,18 +1,24 @@
 import os
+import shutil
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
+# from supervise.pilot_train import DATASET_DIR
+
 IMAGE = cv2.imread('/Users/IMT_Atlantique/project_ia/cars/mysim/data/images/119_cam_image_array_.jpg')
 IMAGE_BNW = cv2.imread('/Users/IMT_Atlantique/project_ia/cars/mysim/data/images/119_cam_image_array_.jpg', cv2.IMREAD_GRAYSCALE)
 
-AFFICHAGE = True
+DATASET_DIR_SOURCE = "/Users/IMT_Atlantique/project_ia/ia_racing_imt/supervise/dataset_drive/"
+DATASET_DIR_TARGET = "/Users/IMT_Atlantique/project_ia/ia_racing_imt/supervise/dataset_drive/preprocessed/"
+
+AFFICHAGE = False
 COLS = 3
 
 def cropY(img, px_from_top):
     if len(np.shape(img)) == 3:
-        return img[px_from_top:img.shape[0], :, :]
-    return img[px_from_top:img.shape[0], :]
+        return img[px_from_top:np.shape(img)[0], :, :]
+    return img[px_from_top:np.shape(img)[0], :]
 
 def gaussian_filter(img, ksize=(3,3), sigmaX = 0):
     return cv2.GaussianBlur(img, ksize=ksize, sigmaX=sigmaX)
@@ -31,8 +37,8 @@ images['IMAGE_BNW'] = gaussian_filter(IMAGE_BNW)
 images['Cropped'] = cropY(IMAGE, 40)
 images['Cropped BW'] = cropY(images['IMAGE_BNW'], 45)
 images['Cropped Gaussien'] = gaussian_filter(images['Cropped'])
-images['Threshold'] = threshold_filter(cv2.cvtColor(images['Cropped Gaussien'], cv2.IMREAD_GRAYSCALE))[1]
-images['Threshold BW'] = threshold_filter(images['Cropped BW'])[1]
+images['Threshold'] = threshold_filter(cv2.cvtColor(images['Cropped Gaussien'], cv2.COLOR_BGR2GRAY))[1]
+# images['Threshold BW'] = threshold_filter(images['Cropped BW'])[1]
 images['Canny'] = canny(images['Cropped Gaussien'])
 
 transfos = images.keys()
@@ -59,3 +65,13 @@ if AFFICHAGE:
 
     plt.show()
 
+
+images_to_preprocess = os.scandir(DATASET_DIR_SOURCE)
+for file in images_to_preprocess:
+    if "jpg" in file.name:
+        img = cv2.imread(file.path, cv2.IMREAD_GRAYSCALE)
+        img = threshold_filter(gaussian_filter(cropY(img, 50)))[1]
+        print(np.shape(img))
+        plt.imsave(DATASET_DIR_TARGET + file.name, img, cmap='gray')
+    elif ".json" in file.name:
+        shutil.copy(file.path, DATASET_DIR_TARGET + file.name)
--- supervise/dataset_drive/label.py
+++ supervise/dataset_drive/label.py
@@ -3,9 +3,9 @@ import json
 import os
 import shutil
 
-TO_CLASSIFY_DIR = os.getcwd() + '/supervise/dataset_drive/a_classer'
+TO_CLASSIFY_DIR = os.getcwd() + '/supervise/dataset_drive2/a_classer'
 prefix = ""
-LABELS_FILE_FILENAME = os.getcwd() + '/supervise/dataset_drive/labels.json'
+LABELS_FILE_FILENAME = os.getcwd() + '/supervise/dataset_drive2/labels.json'
 
 labels_file = open(LABELS_FILE_FILENAME, 'r')
 
@@ -22,15 +22,19 @@ def classify(dir, prefix):
             classify(dir + "/" + file.name, file.name)
             shutil.rmtree(dir + "/" + file.name)
         elif file.is_file():
-            if "jpg" in file.name:
-                shutil.move(dir + "/" + file.name, TO_CLASSIFY_DIR + "/../" + prefix + "-" + file.name)
-            elif "record" in file.name:
+            # if "jpg" in file.name:
+                # shutil.move(dir + "/" + file.name, TO_CLASSIFY_DIR + "/../" + prefix + "-" + file.name)
+            # elif "record" in file.name or "catalog" in file.name:
+            if "record" in file.name or "catalog" in file.name:
                 data = open(file.path, "r")
-                content = json.loads(data.read())
+                contentAll = json.loads(data.read())
                 data.close()
                 
-                labels[prefix + "-" + content['cam/image_array']] = {'user/throttle': content['user/throttle'], 'user/angle': content['user/angle']}
-                
+                for content in contentAll:
+                    if len(prefix) != 0:
+                        labels[prefix + "-" + content['cam/image_array']] = {'user/throttle': content['user/throttle'], 'user/angle': content['user/angle']}
+                    else:
+                        labels[content['cam/image_array']] = {'user/throttle': content['user/throttle'], 'user/angle': content['user/angle']}
 
 if __name__ == "__main__":
     classify(TO_CLASSIFY_DIR, prefix)
